327ba673940a5b884f6dac1b43520284f79293f1
JaviFigueroaVicente <javierfigueroa969@gmail.com>
2025-01-07 19:05:09 +0100

    adaptar database

--- config/dataBase.php
+++ config/dataBase.php
@@ -1,6 +1,6 @@
 <?php
     class DataBase {
-        public static function connect($host='localhost', $user='root', $pass='', $db='mammoth',$port='3306'){
+        public static function connect($host='fdb1029.awardspace.net', $user='4572359_mammothskitchen', $pass='mammoth2025*', $db='4572359_mammothskitchen',$port='3306'){
             $con = new mysqli($host, $user, $pass, $db, $port);
 
             if($con === false){
